9e757eb433329a95ded6bf71ab778e5a855b504c
manq2010 <manq2010@gmail.com>
2023-01-03 17:50:47 +0200

    Add footer

--- src/features/Footer/Footer.jsx
+++ src/features/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Footer = () => {
-  return (
+const Footer = () => (
+  <div>
+
     <div>
-      
+      <p>Created by Mancoba Sihlongonyane</p>
     </div>
-  )
-}
+    <div>
+      <p>
+        Copyright &copy;
+        {' '}
+        {new Date().getFullYear()}
+      </p>
+    </div>
+
+  </div>
+);
 
 export default Footer;
--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import Navbar from '../../features/Navbar/Navbar';
 import headerData from '../../data/headerData';
+import Footer from '../../features/Footer/Footer';
 
 const MainPage = () => (
   <div>
@@ -14,6 +15,8 @@ const MainPage = () => (
       <meta name="description" content="beer lovers application" />
     </Helmet>
     <Navbar />
+
+    <Footer />
 
   </div>
 );
